bf05066b0acce0f3cc272b86799fefd35b5ad6f5
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-07-06 22:44:43 +0545

    Implementing the function of to do list as addition of list, remove of list

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,38 @@
-let username = 'Dave'
-console.log(username)
+import './css/style.css'
+import fullList from './model/fullList'
+import listItem from './model/listItem'
+import listTemplate from './templates/listTemplate'
 
-let a: number = 12
-let b: string = '6'
-let c: number = 2
+const initApp = (): void => {
+  const completeList = fullList.instance
+  const template = listTemplate.instance
 
-console.log(a / b)
+  const itemEntryForm = document.getElementById('itemEntryForm') as HTMLFormElement
+  itemEntryForm.addEventListener('submit', (event: SubmitEvent): void => {
+    event.preventDefault()
 
-console.log(c * b)
+    const input = document.getElementById('newItem') as HTMLInputElement
+    const newEntryText: string = input.value.trim()
+    if(!newEntryText.length) return
+
+    const itemId: number = completeList.list.length ? parseInt(completeList.list[completeList.list.length - 1 ].id) + 1 : 1
+
+    const newItem = new listItem(itemId.toString(), newEntryText)
+
+    completeList.addItem(newItem)
+
+    template.render(completeList)
+  })
+
+  const clearItems = document.getElementById('clearItemsButton') as HTMLButtonElement
+
+  clearItems.addEventListener('click', (): void => {
+    completeList.clearList()
+    template.clear()
+  })
+
+  completeList.load()
+  template.render(completeList)
+}
+
+document.addEventListener('DOMContentLoaded', initApp)
