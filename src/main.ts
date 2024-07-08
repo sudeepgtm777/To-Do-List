fb7ceb3346e4a603c390002084d23953cee7c771
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-07-08 21:47:11 +0545

    Implementing submitEvent textbox

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,46 @@
-import './../src/css/style.css'
-import fullList from './model/fullList'
-import listItem from './model/listItem'
-import listTemplate from './templates/listTemplate'
+import './../src/css/style.css';
+import fullList from './model/fullList';
+import listItem from './model/listItem';
+import listTemplate from './templates/listTemplate';
 
 const initApp = (): void => {
-  const completeList = fullList.instance
-  const template = listTemplate.instance
+  const completeList = fullList.instance;
+  const template = listTemplate.instance;
 
-  const itemEntryForm = document.getElementById('itemEntryForm') as HTMLFormElement
+  const itemEntryForm = document.getElementById(
+    'itemEntryForm'
+  ) as HTMLFormElement;
   itemEntryForm.addEventListener('submit', (event: SubmitEvent): void => {
-    event.preventDefault()
+    event.preventDefault();
 
-    const input = document.getElementById('newItem') as HTMLInputElement
-    const newEntryText: string = input.value.trim()
-    if(!newEntryText.length) return
+    const input = document.getElementById('newItem') as HTMLInputElement;
+    const newEntryText: string = input.value.trim();
+    if (!newEntryText.length) return;
 
-    const itemId: number = completeList.list.length ? parseInt(completeList.list[completeList.list.length - 1 ].id) + 1 : 1
+    const itemId: number = completeList.list.length
+      ? parseInt(completeList.list[completeList.list.length - 1].id) + 1
+      : 1;
 
-    const newItem = new listItem(itemId.toString(), newEntryText)
+    const newItem = new listItem(itemId.toString(), newEntryText);
 
-    completeList.addItem(newItem)
+    completeList.addItem(newItem);
 
-    template.render(completeList)
-  })
+    template.render(completeList);
 
-  const clearItems = document.getElementById('clearItemsButton') as HTMLButtonElement
+    input.value = '';
+  });
+
+  const clearItems = document.getElementById(
+    'clearItemsButton'
+  ) as HTMLButtonElement;
 
   clearItems.addEventListener('click', (): void => {
-    completeList.clearList()
-    template.clear()
-  })
+    completeList.clearList();
+    template.clear();
+  });
 
-  completeList.load()
-  template.render(completeList)
-}
+  completeList.load();
+  template.render(completeList);
+};
 
-document.addEventListener('DOMContentLoaded', initApp)
+document.addEventListener('DOMContentLoaded', initApp);
